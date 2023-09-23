63e2d125bb54dd91be2dd936d21a01b53d743472
_MadushaSandaruwan <44031016+MadushaS@users.noreply.github.com>
2023-09-23 19:17:46 +0530

    data validation pipeline added

--- src/Cogniezer/config/configuration.py
+++ src/Cogniezer/config/configuration.py
@@ -1,5 +1,5 @@
 from Cogniezer.constants import *
-from Cogniezer.entity import DataIngestionConfig
+from Cogniezer.entity import DataIngestionConfig, DataValidationConfig
 from Cogniezer.utils.common import read_yaml,create_directories
 
 class ConfigurationManager:
@@ -26,3 +26,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self)->DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
+        )
+
+        return data_validation_config
---
+++ src/Cogniezer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+from Cogniezer.components.data_validation import DataValidation
+from Cogniezer.config.configuration import ConfigurationManager
+from Cogniezer.logging import logger
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+            config_manager = ConfigurationManager()
+            data_validation_config = config_manager.get_data_validation_config()
+            data_validation = DataValidation(config=data_validation_config)
+            data_validation.validate_all_files()
